22bcf09be83f81b075d7a9b9a0e4b92a1261667c
Louis Liu <liuhanzhong@xiangrikui.com>
2015-12-09 14:55:12 +0800

    完善日志信息

--- lib/rack/xrk/log.rb
+++ lib/rack/xrk/log.rb
@@ -3,7 +3,7 @@ require 'rack/xrk/log/commonlogger'
 module Rack
   module Xrk
     module Log
-      VERSION = '0.1.1'
+      VERSION = '0.1.2'
     end
   end
 end
--- lib/rack/xrk/log/commonlogger.rb
+++ lib/rack/xrk/log/commonlogger.rb
@@ -14,46 +14,46 @@ module Rack
         def log(env, status, header, began_at)
           logger = @logger || env['rack.errors']
 
-          # 1. 过滤静态资源类请求
           return unless header["Content-Type"].present? && (header["Content-Type"].include? "text/html")
 
-          # 2. TODO: 需要传特定的项目值进来
-          application_name = "BXR"
-          log_type = "ACESS"
-
-          controller = env['action_dispatch.request.path_parameters'][:controller]
-          action = env['action_dispatch.request.path_parameters'][:action]
-          response_data_length = env['action_controller.instance'].response_body.to_s.bytesize
+          application_name  = @app.class.parent_name
+          log_type          = "ACESS"
+          client_ip_and_port    = (env['HTTP_X_FORWARDED_FOR'] || env["REMOTE_ADDR"] || "-")
+          response_data_length  = env['action_controller.instance'].response_body.to_s.bytesize
+          query_string          = (env[QUERY_STRING].empty? ? "" : "?"+env[QUERY_STRING])
+          controller            = env['action_dispatch.request.path_parameters'][:controller]
+          action                = env['action_dispatch.request.path_parameters'][:action]
+          return_content        = env['action_controller.instance'].response_body
+          return_content        = (return_content.empty? ? "" : return_content.to_s[0..30])
 
           data = [
-            began_at.to_i,                                    #REQUEST_TIME
-            application_name,                                 #APP_TIME
-            log_type,                                         #LOG_TYPE
-            "#{env['REMOTE_ADDR']}",                          #CLIENT_IP_AND_PORT
-            "#{env['SERVER_NAME']} #{env['SERVER_PORT']}",    #SERVER_IP_AND_PORT
-            (Time.now - began_at).to_i,                       #ELAPSED_TIME
-            "#{env['rack.url_scheme']}",                      #COMMUNICATION_TYPE
-            "REQUEST_DATA_LENGTH",                            #REQUEST_DATA_LENGTH
-            "RESPONSE_DATA_LENGTH",                           #RESPONSE_DATA_LENGTH
+            began_at.to_i,                #REQUEST_TIME
+            application_name,             #APP_TIME
+            log_type,                     #LOG_TYPE
+            client_ip_and_port,           #CLIENT_IP_AND_PORT
+            "#{env['HTTP_HOST']}",        #SERVER_IP_AND_PORT
+            (Time.now - began_at).to_i,   #ELAPSED_TIME
+            "#{env['rack.url_scheme']}",  #COMMUNICATION_TYPE
+            "#{env['CONTENT_LENGTH']}",   #REQUEST_DATA_LENGTH
+            response_data_length,         #RESPONSE_DATA_LENGTH
           ]
 
-          # 3. TODO: 返回类型是String或者JSON，才进行返回 RETURN_CONTENT
           dynamic_params = [
-            "#{env['REQUEST_METHOD']}", #METHOD
-            "#{env['REQUEST_PATH']}",   #URL
-            "#{status}",                #STATUS_CODE
-            "QUERY_ENTITY",             #QUERY_ENTITY
-            "#{controller} #{action}",  #CALL_METHOD
-            "BUSSINESS_CODE",           #BUSSINESS_CODE
-            "RETURN_CONTENT"            #RETURN_CONTENT
+            "#{env['REQUEST_METHOD']}",   #METHOD
+            "#{env['REQUEST_PATH']}",     #URL
+            "#{status.to_s[0..3]}",       #STATUS_CODE
+            query_string,                 #QUERY_ENTITY
+            "#{controller} #{action}",    #CALL_METHOD
+            "",                           #BUSSINESS_CODE
+            return_content                #RETURN_CONTENT
           ]
           data.concat(dynamic_params)
 
 
-          # 4. TODO: 自定义日志输出方法
+          # 1. TODO: 自定义日志输出方法
           logger.info(data.to_s) if logger
 
-          # 5. TODO: Rack和Rails，Grape支持
+          # 2. TODO: Rack和Grape验证
 
         end
       end
