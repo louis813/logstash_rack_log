74a952266021a9435ecf410e452e77b52563019b
Louis Liu <liuhanzhong@xiangrikui.com>
2015-12-09 18:01:54 +0800

    自定义格式支持

--- lib/rack/xrk/log.rb
+++ lib/rack/xrk/log.rb
@@ -3,7 +3,7 @@ require 'rack/xrk/log/commonlogger'
 module Rack
   module Xrk
     module Log
-      VERSION = '0.1.2'
+      VERSION = '0.1.3'
     end
   end
 end
--- lib/rack/xrk/log/commonlogger.rb
+++ lib/rack/xrk/log/commonlogger.rb
@@ -7,7 +7,11 @@ module Rack
       class CommonLogger < Rack::CommonLogger
 
         def initialize(app, logger=nil)
-          logger = ::Logger.new(::File.new("log/app.log","a+"))
+          binding.pry
+          logger = ::Logger.new(::File.new("log/#{app.class.parent_name.downcase}_quality_access.log","a+"))
+          logger.formatter = proc do |severity, datetime, progname, msg|
+             "#{msg}\n"
+          end
           super(app, logger)
         end
 
@@ -16,45 +20,25 @@ module Rack
 
           return unless header["Content-Type"].present? && (header["Content-Type"].include? "text/html")
 
-          application_name  = @app.class.parent_name
-          log_type          = "ACESS"
-          client_ip_and_port    = (env['HTTP_X_FORWARDED_FOR'] || env["REMOTE_ADDR"] || "-")
+          application_name      = @app.class.parent_name
+          log_type              = "ACESS"
+          client_ip_and_port    = (env['HTTP_X_FORWARDED_FOR'] || env['REMOTE_ADDR'] || '-')
           response_data_length  = env['action_controller.instance'].response_body.to_s.bytesize
-          query_string          = (env[QUERY_STRING].empty? ? "" : "?"+env[QUERY_STRING])
+          query_string          = (env['QUERY_STRING'].empty? ? "" : "?"+env['QUERY_STRING'])
           controller            = env['action_dispatch.request.path_parameters'][:controller]
           action                = env['action_dispatch.request.path_parameters'][:action]
           return_content        = env['action_controller.instance'].response_body
-          return_content        = (return_content.empty? ? "" : return_content.to_s[0..30])
+          return_content        = (return_content.empty? ? "" : return_content.to_s[0..99])
 
-          data = [
-            began_at.to_i,                #REQUEST_TIME
-            application_name,             #APP_TIME
-            log_type,                     #LOG_TYPE
-            client_ip_and_port,           #CLIENT_IP_AND_PORT
-            "#{env['HTTP_HOST']}",        #SERVER_IP_AND_PORT
-            (Time.now - began_at).to_i,   #ELAPSED_TIME
-            "#{env['rack.url_scheme']}",  #COMMUNICATION_TYPE
-            "#{env['CONTENT_LENGTH']}",   #REQUEST_DATA_LENGTH
-            response_data_length,         #RESPONSE_DATA_LENGTH
-          ]
+          data = "#{began_at.to_i}, #{application_name}, #{log_type}, #{client_ip_and_port}, #{env['HTTP_HOST']}, #{(Time.now - began_at).to_i}, #{env['rack.url_scheme']}, #{env['CONTENT_LENGTH']}, #{response_data_length},".gsub("\n", "")
 
-          dynamic_params = [
-            "#{env['REQUEST_METHOD']}",   #METHOD
-            "#{env['REQUEST_PATH']}",     #URL
-            "#{status.to_s[0..3]}",       #STATUS_CODE
-            query_string,                 #QUERY_ENTITY
-            "#{controller} #{action}",    #CALL_METHOD
-            "",                           #BUSSINESS_CODE
-            return_content                #RETURN_CONTENT
-          ]
+          dynamic_params = "#{env['REQUEST_METHOD']} #{env['REQUEST_PATH']}, #{status.to_s[0..3]}, #{query_string}, #{controller} #{action}, , #{return_content}".gsub("\n", "")
+
           data.concat(dynamic_params)
 
+          logger.info(data) if logger
 
-          # 1. TODO: 自定义日志输出方法
-          logger.info(data.to_s) if logger
-
-          # 2. TODO: Rack和Grape验证
-
+          # TODO: Rack和Grape验证
         end
       end
     end
